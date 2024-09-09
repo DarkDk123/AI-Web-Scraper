89ebf35fb9c1786fd0961c465e98e0e39b759e25
DarkLord <dipeshrathore122@gmail.com>
2024-09-09 18:18:44 +0530

    Enhance UI, add URL validation, and integrate scraping functions with Streamlit

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,13 +1,33 @@
+import re
 import streamlit as st
-
+from scrape import (
+    scrape_website,
+    extract_body_content,
+    clean_body_content,
+    split_dom_content,
+)
 
 # UI
 st.title("AI Web Scraper 🤖")
-st.subheader("This is an example of an AI web scraper.")
+st.subheader("An AI web scraper for your simple daily scraping needs!")
 
-url = st.text_input("Enter the URL of the website you want to scrape")
+url = st.text_input("URL of the website you want to scrape")
+button = st.button("Start scraping 🏌🏽")
 
-if st.button("Scrape"):
-    st.write("Scraping...")
 
-st.write("Thanks for using this app!")
+if button:
+    if not url or re.match(r"^(?:http|ftp)s?://([^/]+[.])*[^/]+/?.*$", url) is None:
+        st.error("Please Enter a valid URL!")
+    else:
+        st.write("Scraping the website...")
+
+        # Scrape the website
+        html_page = scrape_website(url)
+        html_body = extract_body_content(html_page)
+        cleaned_content = clean_body_content(html_body)
+
+        st.session_state["dom_content"] = cleaned_content
+
+        with st.expander("View DOM Content 👁️"):
+            st.text_area("DOM Content", cleaned_content, height=300)
+
