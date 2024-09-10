182b5d089463a5e61bfcececdab4027f86afd705
DarkLord <dipeshrathore122@gmail.com>
2024-09-10 16:50:12 +0530

    Streamlit UI integrations with the parser | Project completed

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,9 +1,11 @@
 import re
 import streamlit as st
+
+from parse_LLM import parse_chunks_with_HF_LLM
 from scrape import (
-    scrape_website,
-    extract_body_content,
     clean_body_content,
+    extract_body_content,
+    scrape_website,
     split_dom_content,
 )
 
@@ -12,22 +14,34 @@ st.title("AI Web Scraper 🤖")
 st.subheader("An AI web scraper for your simple daily scraping needs!")
 
 url = st.text_input("URL of the website you want to scrape")
-button = st.button("Start scraping 🏌🏽")
+button = st.button("Start scraping 👷🪓")
 
-
-if button:
-    if not url or re.match(r"^(?:http|ftp)s?://([^/]+[.])*[^/]+/?.*$", url) is None:
+url_regex = re.match(r"^(?:http|ftp)s?://([^/]+[.])*[^/]+/?.*$", url)
+if button and (not url or url_regex is None):
         st.error("Please Enter a valid URL!")
-    else:
-        st.write("Scraping the website...")
 
-        # Scrape the website
-        html_page = scrape_website(url)
-        html_body = extract_body_content(html_page)
-        cleaned_content = clean_body_content(html_body)
+if button and url and url_regex:
+    st.write("Scraping the website...")
 
-        st.session_state["dom_content"] = cleaned_content
+    # Scrape the website
+    html_page = scrape_website(url)
+    html_body = extract_body_content(html_page)
+    cleaned_content = clean_body_content(html_body)
 
-        with st.expander("View DOM Content 👁️"):
-            st.text_area("DOM Content", cleaned_content, height=300)
+    st.session_state["dom_content"] = cleaned_content
 
+    with st.expander("View DOM Content 👁️"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_query = st.text_area("What do you want to parse?")
+
+    if st.button("Parse ⚒️"):
+        chunks = split_dom_content(st.session_state["dom_content"])
+        st.write("Parsing the content...")
+
+        # Parse the content with an LLM
+        dom_chunks = split_dom_content(st.session_state.dom_content)
+        parsed_result = parse_chunks_with_HF_LLM(dom_chunks, parse_query)
+        st.write(parsed_result)
